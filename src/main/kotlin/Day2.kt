99f0c9f55182b9f01c4604f84bda395e6355d9d9
brandonb <brandon.belcher@oracle.com>
2020-12-02 12:30:27 +0000

    toInt

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -3,8 +3,8 @@ object Day2 {
     fun runPart1(input: List<String>): Int {
         return input.count {
             val parse = it.split("-", ": ", " ")
-            val minCount = Integer.parseInt(parse[0])
-            val maxCount = Integer.parseInt(parse[1])
+            val minCount = parse[0].toInt()
+            val maxCount = parse[1].toInt()
             val validator = parse[2][0]
 
             val count = parse[3].count { character -> character == validator }
@@ -15,8 +15,8 @@ object Day2 {
     fun runPart2(input: List<String>): Int {
         return input.count {
             val parse = it.split("-", ": ", " ")
-            val index1 = Integer.parseInt(parse[0]) - 1
-            val index2 = Integer.parseInt(parse[1]) - 1
+            val index1 = parse[0].toInt() - 1
+            val index2 = parse[1].toInt() - 1
             val validator = parse[2][0]
 
             (validator == parse[3][index1]).xor(validator == parse[3][index2])

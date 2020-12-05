7c646ce5454323cba44aabc57ea192584cbade20
brandonb <brandon.belcher@oracle.com>
2020-12-05 17:30:15 +0000

    Day5

--- src/main/kotlin/Day5.kt
+++ src/main/kotlin/Day5.kt
@@ -1,16 +1,15 @@
 object Day5 {
 
-    fun runPart1(input: List<String>) = input.maxOf { calculateSeatId(it) }
+    fun runPart1(input: List<String>) =
+        input.maxOf(::calculateSeatId)
 
-    fun runPart2(input: List<String>): Int {
-        return input.asSequence().map { calculateSeatId(it) }.sorted().windowed(size = 2, step = 1).first {
+    fun runPart2(input: List<String>) =
+        input.map(::calculateSeatId).sorted().windowed(size = 2).first {
             it[0] + 1 != it[1]
         }.first() + 1
-    }
 
-    fun calculateSeatId(input: String): Int {
-        return input.map {
+    fun calculateSeatId(input: String) =
+        input.map {
             if (it == 'B' || it == 'R') 1 else 0
         }.joinToString("").toInt(2)
-    }
 }
